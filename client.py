3304d6bc0846156ee23488faa76382ac4a6bf61b
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2020-12-13 10:45:54 -0300

    Update client.py

--- client.py
+++ client.py
@@ -11,14 +11,14 @@ def list_imgs(rootdir):
 			with open(current + '/' + f, "rb") as handle:
 				binary_data = xmlrpclib.Binary(handle.read())
 			proxy.server_receive_file(binary_data, f)
-	print "Imagens enviadas pelo client com sucesso! Aguardando o inicio do processamento de imagem pelo servidor."
+	print "Imagens enviadas pelo client com sucesso! Aguardando o inicio do image splitting and merging bands pelo servidor."
 	start = sucuri(int(sys.argv[1]))
 
 def sucuri(nprocs):
 	proxy.sucuri(nprocs)
-	print "Processamento de imagens iniciado. Aguardando o termino do reconhecimento facial pelo servidor..."
+	print "Processamento de imagens iniciado. Aguardando o termino do do image splitting and merging bands pelo servidor."
 
 t0 = time.time()
 imagePath = list_imgs(sys.argv[2])
 t1 = time.time()
-print "Processamento terminado. Tempo total do reconhecimento facial das imagens = %.3f" %(t1-t0)
+print "Processamento terminado. Tempo total do processamento das imagens = %.3f" %(t1-t0)
